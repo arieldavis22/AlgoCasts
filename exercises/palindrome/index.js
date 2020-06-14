04778d5d473908da63d9565e539db9c52c3c7008
arieldavis22 <aadavisbusiness@gmail.com>
2020-06-14 12:08:45 -0400

    done

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,18 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    return str.split('').every((char, i) => {
+        return char === str[str.length - i - 1]
+    })
+}
 
 module.exports = palindrome;
+
+
+//function palindrome(str) {
+    //     const reversedString = str.split('').reduce((rev, char) => {
+    //         return char + rev
+    //     }, '')
+    //     return reversedString === str
+    // }
