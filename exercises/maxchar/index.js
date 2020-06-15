aa72b29d1c04e1ba2d8cc956089f469ea4abedbb
arieldavis22 <aadavisbusiness@gmail.com>
2020-06-15 17:48:12 -0400

    done

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,30 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const obj = {};
+    let max = '';
+    let num = 0;
+
+
+    for (let char of str) {
+        obj[char] = obj[char] + 1 || 1
+    }
+
+    // Object.keys(obj).forEach((key) => {
+    //     if(obj[key] > num) {
+    //         num = obj[key]
+    //         max = key
+    //     }
+    // })
+    for (let char in obj) {
+        if(obj[char] > num) {
+            num = obj[char]
+            max = char
+        }
+    }
+
+    return max
+}
 
 module.exports = maxChar;
