ef5b9eb0f5bc3d16b8c7593b43cd63bc964c04ab
arieldavis22 <aadavisbusiness@gmail.com>
2020-06-20 13:09:00 -0400

    done

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,30 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    const chunked = []
+    let i = 0
+
+    while (i < array.length) {
+        chunked.push(array.slice(i, i + size))
+        i += size
+    }
+    return chunked
+}
 
 module.exports = chunk;
+
+// function chunk(array, size) {
+//     const chunked = []
+
+//     array.forEach(el => {
+//         const last = chunked[chunked.length - 1] //get last element (in an array)
+
+//         if(!last || last.length === size) { //check to see if last element is undifined and if size is same as size given
+//             chunked.push([el]) //if the last array is finish, push it onto the chunked array
+//         } else {
+//             last.push(el)
+//         }
+//     })
+//     return chunked
+// }
